88b2e9f660c13d943efb5ea1d2a9fa093a619571
bat9k <ilia34775@gmail.com>
2024-03-14 13:49:25 +0300

    create horizontalScroll

--- app/src/main/java/com/raywenderlich/android/lab1/screens/ScrollingScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/ScrollingScreen.kt
@@ -3,9 +3,11 @@ package com.raywenderlich.android.lab1.screens
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ImageBitmap
@@ -16,6 +18,7 @@ import androidx.compose.ui.unit.dp
 import com.raywenderlich.android.lab1.router.BackButtonHandler
 import com.raywenderlich.android.lab1.router.FundamentalsRouter
 import com.raywenderlich.android.lab1.router.Screen
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -33,7 +36,7 @@ fun ScrollingScreen () {
 
 @Composable
 fun MyScrollingScreen(modifier: Modifier = Modifier) {
-    Column(modifier = modifier.verticalScroll(rememberScrollState())){
+    Row(modifier = modifier.horizontalScroll(rememberScrollState())){
         BookImage(R.drawable.advanced_architecture_android, R.string.architecture)
         BookImage(R.drawable.kotlin_aprentice, R.string.learn_kotlin)
         BookImage(R.drawable.kotlin_coroutines, R.string.coroutines)
